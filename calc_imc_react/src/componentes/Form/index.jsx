0383a7b348746b6326cb240a480ed2d1bddf629c
Rafael Gonçalves Ribeiro <raffafullstack@gmail.com>
2025-01-20 17:09:42 -0300

    completando cards, adicionando mecânica de card selecionado e estilização

--- calc_imc_react/src/componentes/Form/index.jsx
+++ calc_imc_react/src/componentes/Form/index.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import { useState, useEffect } from "react";
 import styles from './form.module.css';
 
+import imc1 from '../../img/imc1.png'
+import imc2 from '../../img/imc2.png'
+import imc3 from '../../img/imc3.png'
+import imc4 from '../../img/imc4.png'
+import imc5 from '../../img/imc5.png'
+import imc6 from '../../img/imc6.png'
+
 export default()=>{
     const [peso, setPeso] = useState("")
     const [altura, setAltura] = useState("")
@@ -26,33 +33,33 @@ export default()=>{
                     </div>
                 </div>
                 <div className={styles.IMCbox}>
-                    <div>
-                        <img src="" alt="" />
+                    <div className={`${styles.niveis} ${IMC != 0 && IMC < 18.5 ? styles.selecionado : ''}`}>
+                        <img src={imc1} alt="imc1" />
                         <h3>Abaixo do Peso</h3>
                         <p className={styles.texto}><strong>IMC menor que 18,5. Indica peso insuficiente para a altura, podendo estar associado a deficiências nutricionais ou problemas de saúde</strong></p>
                     </div>
-                    <div>
-                        <img src="" alt="" />
+                    <div className={`${styles.niveis} ${IMC >= 18.5 && IMC <= 24.9 ? styles.selecionado : ''}`}>
+                        <img src={imc2} alt="imc2" />
                         <h3>Peso Normal</h3>
                         <p className={styles.texto}><strong>IMC entre 18,5 e 24,9. Considerado saudável, indicando equilíbrio entre peso e altura.</strong></p>
                     </div>
-                    <div>
-                        <img src="" alt="" />
+                    <div className={`${styles.niveis} ${IMC >= 25 && IMC <= 29.9 ? styles.selecionado : ''}`}>
+                        <img src={imc3} alt="imc3" />
                         <h3>Sobrepeso</h3>
                         <p className={styles.texto}><strong>IMC entre 25,0 e 29,9. Peso acima do ideal, podendo representar risco aumentado para doenças crônicas, como hipertensão.</strong></p>
                     </div>
-                    <div>
-                        <img src="" alt="" />
+                    <div className={`${styles.niveis} ${IMC >= 30 && IMC <= 34.9 ? styles.selecionado : ''}`}>
+                        <img src={imc4} alt="imc4" />
                         <h3>Obesidade grau I</h3>
                         <p className={styles.texto}><strong>IMC entre 30,0 e 34,9. Excesso de peso com maior risco de doenças relacionadas, como diabetes tipo 2 e problemas cardiovasculares.</strong></p>
                     </div>
-                    <div>
-                        <img src="" alt="" />
+                    <div className={`${styles.niveis} ${IMC >= 35 && IMC <= 39.9 ? styles.selecionado : ''}`}>
+                        <img src={imc5} alt="imc5" />
                         <h3>Obesidade grau II</h3>
                         <p className={styles.texto}><strong>IMC entre 35,0 e 39,9. Condição mais grave, associada a riscos significativos para a saúde e maior probabilidade de complicações.</strong></p>
                     </div>
-                    <div>
-                        <img src="" alt="" />
+                    <div className={`${styles.niveis} ${IMC >= 40 ? styles.selecionado : ''}`}>
+                        <img src={imc6} alt="imc6" />
                         <h3>Obesidade grau III</h3>
                         <p className={styles.texto}><strong>IMC igual ou maior que 40,0. Nível mais severo, com alto risco de doenças graves e impacto significativo na qualidade de vida.</strong></p>
                     </div>

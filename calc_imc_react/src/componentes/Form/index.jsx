03fb539747f3823b6c5bcd0736b53e9a9759fc59
Rafael Gonçalves Ribeiro <raffafullstack@gmail.com>
2025-01-20 16:54:28 -0300

    criando formulário e definindo seu funcionamento

--- calc_imc_react/src/componentes/Form/index.jsx
+++ calc_imc_react/src/componentes/Form/index.jsx
@@ -15,7 +15,17 @@ export default()=>{
 
     return(
         <div className={styles.allBox}>
-            
+            <div className={styles.contentBox}>
+                <h1>Como está sua saúde?</h1>
+                <div className={styles.formBox}>
+                    <h2>Calcule seu IMC aqui:</h2>
+                    <input type="number" placeholder="Seu peso em kg" onChange={e => setPeso(Number(e.target.value))} onKeyUp={resultado} />
+                    <input type="number" placeholder="Sua altura em cm" onChange={e => setAltura(Number(e.target.value))} onKeyUp={resultado} />
+                    <div className={styles.resultBox}>
+                        {peso && altura&& (<p>Seu IMC é de: {IMC}</p>)}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }

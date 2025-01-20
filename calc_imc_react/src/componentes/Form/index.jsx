9c038aade02e71dbb41019ab01c1652399c0858a
Rafael Gonçalves Ribeiro <raffafullstack@gmail.com>
2025-01-20 16:53:03 -0300

    descrevendo dados a serem utilizados

--- calc_imc_react/src/componentes/Form/index.jsx
+++ calc_imc_react/src/componentes/Form/index.jsx
@@ -3,6 +3,15 @@ import { useState, useEffect } from "react";
 import styles from './form.module.css';
 
 export default()=>{
+    const [peso, setPeso] = useState("")
+    const [altura, setAltura] = useState("")
+    const [IMC, setIMC] = useState(0)
+
+    function resultado(){
+        const alturaM = altura/100
+        const IMCcompleto = (peso/(alturaM*alturaM)).toFixed(1)
+        setIMC(IMCcompleto)
+    }
 
     return(
         <div className={styles.allBox}>
